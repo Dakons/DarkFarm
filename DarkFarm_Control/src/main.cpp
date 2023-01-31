bdb61b05abfb5bc308f461a047859255bf6d6ec8
Dakons <savinov.dima44@yandex.ru>
2023-01-31 18:17:33 +0300

    V 1.0 - Первая версия, самоРабота
    Поливает по пресету
    Светит по пресету
    
    Нет связи с инетом, находится сам в себе.

--- DarkFarm_Control/src/main.cpp
+++ DarkFarm_Control/src/main.cpp
@@ -6,26 +6,67 @@
 #define SDA_TIMER 10
 #define SCL_TIMER 11
 
+
+// Обозначаем пины реле для воды и света
+#define LIGHT_POWER 9
+#define WATER_POWER 8
+
+
+unsigned long TimeNow; //Для таймера полива
+
+bool Watering_Flag = 0; //Флаг полива. 0 - поливать не надо. 1 - поливать надо 
+
+
 MicroDS3231 rtc; // Создаем объект таймера
 
 struct Light_Preset //Структура для пресета света
 {
 
-byte FirstTimeMarkerWork;  //Когда свет включается
-byte SecondTimeMarkerWork; //Когда свет выключается
+byte FirstTimeMarker;  //Когда свет включается
+byte SecondTimeMarker; //Когда свет выключается
 
 };
+
+ Light_Preset LP_FIRST; //Создаем структуру пресета для света
 
 struct Water_Preset //Структура для пресета полива
 {
 
-byte Period;  //периодичность включений
-byte Duration; //длительность полива
+byte Period;  //периодичность включений Часы
+byte Duration; //длительность полива Секунды
+bool WateringStart; //Включать ли полив при запуске кода
 
 };
 
+Water_Preset WP_FIRST;
+
+byte LastWaterTime; //Для сохранение времени последнего полива
+
+
+
+
+
+
+/////////////////Прототипы функций
+
+void LightControl();
+
+void WaterControl();
+
+void Watering();
+//////////////////
+
 void setup()
 {
+
+    //Настраиваем пресет света
+LP_FIRST.FirstTimeMarker = 9;
+LP_FIRST.SecondTimeMarker = 21;
+   
+
+   //Настраиваем пресет Полива
+WP_FIRST.Period = 1;
+WP_FIRST.Duration = 15;
 
     Serial.begin(115200);
     // Запускаем таймер, устанавливаем время
@@ -38,11 +79,64 @@ void setup()
         Serial.println("Таймер не найден");
     }
 
+LastWaterTime = rtc.getHours();
+TimeNow = millis();
+Watering_Flag = WP_FIRST.WateringStart; //Включаем полив или не включаем полив
 
 }
 
 
+
 void loop()
 {
+
+LightControl();
+
+WaterControl();
+
+Watering();
+
+
+}
+
+
+
+///Функции
+
+void LightControl()
+{
+    if (rtc.getHours() >= LP_FIRST.FirstTimeMarker && rtc.getHours() <= LP_FIRST.SecondTimeMarker)
+    {
+        digitalWrite(LIGHT_POWER, HIGH);
+    }
+
+    else
+
+    {
+        digitalWrite(LIGHT_POWER, LOW);
+    }
+}
+
+
+void WaterControl() //Контроль полива
+{
+    if (rtc.getHours() - LastWaterTime >= WP_FIRST.Period) //Проверяем прошло ли время периода
+    {
+        LastWaterTime = rtc.getHours(); //  нынешнее время ставим
+
+        digitalWrite(WATER_POWER, HIGH); //Включаем полив
+        TimeNow = millis();//Фиксируем начало полива
+        Watering_Flag = 1; //Включаем флаг полива
+
+    }
+}
+
+void Watering() //Выполнение полива
+{
+    if ((millis() - TimeNow > WP_FIRST.Duration * 1000) && Watering_Flag == 1)
+    {
+        digitalWrite(WATER_POWER, LOW);
+        Watering_Flag = 0;
+    }
 
 }
